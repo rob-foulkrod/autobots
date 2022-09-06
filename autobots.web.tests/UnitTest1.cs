bcf0a19f66888b5153f39ad889cb36ed889f6ceb
Rob Foulkrod <rob foulkrod>
2022-09-06 14:52:17 -0400

    Initial Project structure #4158

---
+++ autobots.web.tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace autobots.web.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ autobots.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace autobots.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
